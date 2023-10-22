b74410b16d308bad77bd6067003c185d9c135c55
seastamp <seastamp10@gmail.com>
2023-10-22 16:13:12 +0900

    Feat: KioskStatus 클래스 추가

--- src/KioskStatus.java
+++ src/KioskStatus.java
@@ -1,7 +1,8 @@
 public class KioskStatus {
-    public final int MAIN = 1;
-    public final int PRODUCT = 2;
-    public final int CART = 3;
-    public final int CANCEL = 4;
-    public final int COMPLETE = 5;
+    public final int MAIN_MENU = 1;
+    public final int PRODUCT_MENU = 2;
+    public final int PRODUCT_ADD = 3;
+    public final int CART = 4;
+    public final int ORDER_CANCEL = 5;
+    public final int ORDER_COMPLETE = 6;
 }
